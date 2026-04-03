6011f1987e604c0f8535695f7c4cff63c9cda484
David Villamizar <villamizarandresdavid@gmail.com>
2026-04-03 13:08:51 -0400

    feat: extender limitantes seguras a todos los scripts

--- 4_escanear_videos.bat
+++ 4_escanear_videos.bat
@@ -4,6 +4,6 @@ echo.
 echo Iniciando entorno Docker para escanear videos...
 echo Puede presionar Ctrl+C para detener el proceso en cualquier momento.
 echo.
-docker run --rm -it -v "%~dp0:/app" py_faces_app python escaner_videos.py
+docker run --rm -it --memory=12g --cpus=14 -v "%~dp0:/app" py_faces_app python escaner_videos.py
 echo.
 pause
